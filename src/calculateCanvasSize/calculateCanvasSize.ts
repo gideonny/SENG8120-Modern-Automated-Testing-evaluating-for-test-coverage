411d226ccb05ab2aad30f0e57aba63e7837e8978
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-06-15 00:12:39 -0400

    feat: support decimal values in calculateCanvasSize

--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -1,6 +1,6 @@
 export default function calculateCanvasSize(length: string, width: string) {
-  const parsedLength = parseInt(length);
-  const parsedWidth = parseInt(width);
+  const parsedLength = parseFloat(length);
+  const parsedWidth = parseFloat(width);
 
   if (isNaN(parsedLength) || isNaN(parsedWidth)) {
     throw new Error("Invalid input: length and width must be numbers");
