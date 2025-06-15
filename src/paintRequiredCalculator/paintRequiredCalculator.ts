be2cae2b07f0e54a472226022eaff1ad9f5c008f
Gideon Nyamuame <104134288+gideonny@users.noreply.github.com>
2025-06-15 18:10:03 -0400

    feat: Added code to handle negative area

--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -2,5 +2,9 @@ export default function paintRequiredCalculator(area: number, coveragePerLiter: 
   if (coveragePerLiter <= 0) {
     throw new Error("Coverage per liter must be greater than zero");
   }
+
+   if (area < 0) {
+    throw new Error("Area must be a positive number");
+  }
   return area / coveragePerLiter;
 }
